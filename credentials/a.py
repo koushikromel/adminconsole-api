693ddfc31dc91a294190e78016a3a9846259484a
sanjeev929 <sanjeevsanju929@gmail.com>
2023-05-23 18:11:31 +0530

    add function

--- credentials/a.py
+++ credentials/a.py
@@ -3,7 +3,12 @@ import inspect
 
 # a = requests.post(url="http://127.0.0.1:8118/absentees", data={"test":"data"})
 # a = requests.post(url="http://127.0.0.1:8118/absentees/fordate", json={"date": "2023-05-19", "b":12})
-a = requests.get(url="http://127.0.0.1:8118/absentees")
+
+# a = requests.get(url="http://127.0.0.1:8118/inventory")
+# print(a.json())
+
+# a = requests.post(url="http://127.0.0.1:8118/inventory/id", json={"id": "ON30A1CH"})
+# print(a.json())
 
 # result = a.json()['info']['description']
 # print('desc s', result)
@@ -15,9 +20,17 @@ a = requests.get(url="http://127.0.0.1:8118/absentees")
 #     return f"Added sum is {num1+num2}"
 
 # print(add_two_numbers("1","2"))
-response = requests.get('http://localhost:8118/absentees')  # Replace with your endpoint URL
+# response = requests.get('http://localhost:8118/absentees')  # Replace with your endpoint URL
 
-example_endpoint = inspect.getmembers(response.json(), inspect.isfunction)[0][1]
-docstring = example_endpoint.__doc__
+# example_endpoint = inspect.getmembers(response.json(), inspect.isfunction)[0][1]
+# docstring = example_endpoint.__doc__
 
-print(docstring)
+# print(docstring)
+
+
+
+# a = requests.get(url="http://127.0.0.1:8118/customer")
+# print(a.json())
+
+a = requests.post(url="http://127.0.0.1:8118/quotationandinvoice/invoice/year", json={"year":"2022"})
+print(a.json())
